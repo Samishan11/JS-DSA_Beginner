4ae654300c065419b81a89f5fe276508a6b5ea57
Samishan11 <samishanthapa0@gmail.com>
2023-08-18 20:19:49 +0545

    change

--- callback.js
+++ callback.js
@@ -1,6 +1,24 @@
-function timeOut(callback) {
-  return setTimeout(() => console.log("1000"), 1000);
+//  here we directly call the fuction timeout
+function callbackFunction() {
+  return console.log("Hello");
+  timeOut();
+}
+
+function timeOut() {
+  setTimeout(() => console.log("2000"), 2000);
+}
+
+callbackFunction();
+
+//  here is the prefect example of callback function
+//  here we pass the timeOut function as a call back and call the function
+function callbackFunction(callback) {
+  return console.log("Hello");
   callback();
 }
 
-timeOut(setTimeout(() => console.log("2000"), 2000));
+function timeOut() {
+  setTimeout(() => console.log("2000"), 2000);
+}
+
+callbackFunction(timeOut());
